0840889390d55e23048bad545800a31591ed0de7
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-02 13:21:11 +0200

    feat: Add registry secrets to rancher projects

--- bar.tf
+++ bar.tf
@@ -3,16 +3,20 @@ module "bar_harbor" {
   project_name = "bar"
 }
 
-module "bar_project" {
-  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
-
-  name       = "bar"
-  cluster_id = module.cluster_info.cluster_id
-}
-
 module "bar_harbor_robot_account" {
   source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//harbor_robot_account?ref=master"
 
   project_name       = module.bar_harbor.project_name
   robot_account_name = "bar"
+}
+
+module "bar_project" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
+
+  name       = "bar"
+  cluster_id = module.cluster_info.cluster_id
+
+  registry          = "harbor.k8s.sikademo.com"
+  registry_username = module.bar_harbor_robot_account.robot_username
+  registry_password = module.bar_harbor_robot_account.robot_password
 }
--- foo.tf
+++ foo.tf
@@ -3,16 +3,20 @@ module "foo_harbor" {
   project_name = "foo"
 }
 
-module "foo_project" {
-  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
-
-  name       = "foo"
-  cluster_id = module.cluster_info.cluster_id
-}
-
 module "foo_harbor_robot_account" {
   source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//harbor_robot_account?ref=master"
 
   project_name       = module.foo_harbor.project_name
   robot_account_name = "foo"
+}
+
+module "foo_project" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
+
+  name       = "foo"
+  cluster_id = module.cluster_info.cluster_id
+
+  registry          = "harbor.k8s.sikademo.com"
+  registry_username = module.foo_harbor_robot_account.robot_username
+  registry_password = module.foo_harbor_robot_account.robot_password
 }
