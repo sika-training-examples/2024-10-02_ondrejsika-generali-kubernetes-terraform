0a9511332e598ffeab8f75d0567e19105e2eae2e
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-02 11:44:04 +0200

    feat(bar): Create harbor project & robot account

--- bar.tf
+++ bar.tf
@@ -1,6 +1,18 @@
+module "bar_harbor" {
+  source       = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//harbor_project?ref=master"
+  project_name = "bar"
+}
+
 module "bar_project" {
   source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
 
   name       = "bar"
   cluster_id = module.cluster_info.cluster_id
+}
+
+module "bar_harbor_robot_account" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//harbor_robot_account?ref=master"
+
+  project_name       = module.bar_harbor.project_name
+  robot_account_name = "bar"
 }
