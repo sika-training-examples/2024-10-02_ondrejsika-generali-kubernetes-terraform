571b273acd198437c078f5ee69fa969348c78016
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-02 10:59:29 +0200

    feat: Get cluster id from cluster_info module

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
+module "cluster_info" {
+  source       = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//cluster_info?ref=master"
+  cluster_name = "local"
+}
+
 module "foo_project" {
   source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
 
   name       = "foo"
-  cluster_id = "local"
+  cluster_id = module.cluster_info.cluster_id
 }
 
 module "bar_project" {
   source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
 
   name       = "foo"
-  cluster_id = "local"
+  cluster_id = module.cluster_info.cluster_id
 }
