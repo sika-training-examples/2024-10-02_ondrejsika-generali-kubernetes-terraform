5a47d2384849c3ed90dc74701949cf3621a75292
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-02 11:43:38 +0200

    fix(bar): fix name

--- bar.tf
+++ bar.tf
@@ -1,6 +1,6 @@
 module "bar_project" {
   source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//rancher_project?ref=master"
 
-  name       = "foo"
+  name       = "bar"
   cluster_id = module.cluster_info.cluster_id
 }
